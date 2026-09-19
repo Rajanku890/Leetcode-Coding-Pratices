bbcbbddd9a3e0a97229ce3ff058ca5ef9cd281ee
Rajan kumar yadav <yadavrajan808426@gmail.com>
2026-09-19 00:23:55 -0400

    Time: 4 ms (89.52%), Space: 46.3 MB (35.13%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,20 +1,44 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        if(nums == null || nums.length == 0) {
-            return 0;
-        }
-        int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        int[][] v1 = new int[nums.length][2];
+
+        int size = 0;
+        int i = 0;
+
+        for (int a : nums) {
+
+            if (size == 0 || a > v1[size - 1][0]) {
+                v1[size][0] = a;
+                v1[size][1] = i;
+                size++;
+
+            } else {
+
+                int l = 0;
+                int h = size - 1;
+                int id = -1;
+                
+                while (l <= h) {
+                    int mid = (l + h) / 2;
+
+                    if (v1[mid][0] >= a) {
+                        id = mid;
+                        h = mid - 1;
+                    } else {
+                        l = mid + 1;
+                    }
+                }
+
+                if (i > v1[id][1]) {
+                    v1[id][0] = a;
+                    v1[id][1] = i;
                 }
             }
+
+            i++;
         }
-        int maxLength = Arrays.stream(dp).max().orElse(0);
-        return maxLength;
+
+        return size;
     }
 }
