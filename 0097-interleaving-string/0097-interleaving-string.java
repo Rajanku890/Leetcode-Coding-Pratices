dca4911587c87ca6257d425ad8d6723c80ddbb52
Rajan kumar yadav <yadavrajan808426@gmail.com>
2026-09-23 05:54:01 -0400

    Time: 3 ms (75.76%), Space: 43.1 MB (79.7%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,30 +1,49 @@
 class Solution {
-    public boolean isInterleave(String s1, String s2, String s3) {
-        int m = s1.length();
-        int n = s2.length();
 
-        if (m + n != s3.length()) {
-            return false;
+    int[][] dp;
+
+    int fun(int i, int j, String s1, String s2, String s3) {
+        if (i == s1.length() && j == s2.length()) {
+            return 1;
         }
 
-        boolean[][] dp = new boolean[m + 1][n + 1];
-        dp[0][0] = true;
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
 
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
+        int k = i + j;
+        if (i < s1.length() &&
+            s1.charAt(i) == s3.charAt(k)) {
 
-                if (i > 0 &&
-                    s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
-                    dp[i][j] |= dp[i - 1][j];
-                }
+            if (fun(i + 1, j, s1, s2, s3) == 1) {
+                return dp[i][j] = 1;
+            }
+        }
+        if (j < s2.length() &&
+            s2.charAt(j) == s3.charAt(k)) {
 
-                if (j > 0 &&
-                    s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
-                    dp[i][j] |= dp[i][j - 1];
-                }
+            if (fun(i, j + 1, s1, s2, s3) == 1) {
+                return dp[i][j] = 1;
             }
         }
 
-        return dp[m][n];
+        return dp[i][j] = 0;
+    }
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+
+        dp = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return fun(0, 0, s1, s2, s3) == 1;
     }
 }
