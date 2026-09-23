4274141b3267c6ad7910b96ca0a5a32b0459ea59
Rajan kumar yadav <yadavrajan808426@gmail.com>
2026-09-23 05:55:30 -0400

    Time: 186 ms (5.5%), Space: 283 MB (5.01%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,49 +1,69 @@
 class Solution {
 
-    int[][] dp;
+    int[][][] dp = new int[101][101][201];
 
-    int fun(int i, int j, String s1, String s2, String s3) {
-        if (i == s1.length() && j == s2.length()) {
+    int fun(int i, int j, int k, String s, String t, String str) {
+        if (i >= s.length()) {
+
+            while (j < t.length() && k < str.length()) {
+                if (t.charAt(j) == str.charAt(k)) {
+                    j++;
+                    k++;
+                } else {
+                    return 0;
+                }
+            }
+
             return 1;
         }
+        if (j >= t.length()) {
 
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int k = i + j;
-        if (i < s1.length() &&
-            s1.charAt(i) == s3.charAt(k)) {
-
-            if (fun(i + 1, j, s1, s2, s3) == 1) {
-                return dp[i][j] = 1;
+            while (i < s.length() && k < str.length()) {
+                if (s.charAt(i) == str.charAt(k)) {
+                    i++;
+                    k++;
+                } else {
+                    return 0;
+                }
             }
-        }
-        if (j < s2.length() &&
-            s2.charAt(j) == s3.charAt(k)) {
 
-            if (fun(i, j + 1, s1, s2, s3) == 1) {
-                return dp[i][j] = 1;
-            }
+            return 1;
+        }
+        if (dp[i][j][k] != -1) {
+            return dp[i][j][k];
         }
 
-        return dp[i][j] = 0;
+        int m = 0;
+        if (s.charAt(i) == str.charAt(k)) {
+            int a = fun(i + 1, j, k + 1, s, t, str);
+            m = m | a;
+        }
+        if (t.charAt(j) == str.charAt(k)) {
+            int b = fun(i, j + 1, k + 1, s, t, str);
+            m = m | b;
+        }
+
+        if (s.charAt(i) != str.charAt(k) &&
+            t.charAt(j) != str.charAt(k)) {
+            return 0;
+        }
+
+        return dp[i][j][k] = m;
     }
 
     public boolean isInterleave(String s1, String s2, String s3) {
-
         if (s1.length() + s2.length() != s3.length()) {
             return false;
         }
 
-        dp = new int[s1.length() + 1][s2.length() + 1];
-
-        for (int i = 0; i <= s1.length(); i++) {
-            for (int j = 0; j <= s2.length(); j++) {
-                dp[i][j] = -1;
+        for (int i = 0; i < 101; i++) {
+            for (int j = 0; j < 101; j++) {
+                for (int k = 0; k < 201; k++) {
+                    dp[i][j][k] = -1;
+                }
             }
         }
 
-        return fun(0, 0, s1, s2, s3) == 1;
+        return fun(0, 0, 0, s1, s2, s3) == 1;
     }
 }
